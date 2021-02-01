0ece6c27694cf470732087f04854f928999be30f
Daniel Koska <danielkoska86@hotmail.com>
2021-02-01 16:28:45 +0100

    Add function that converts discrete time series to functional data

--- R/main.R
+++ R/main.R
@@ -1,7 +1,15 @@
 # Main script Functional Limits of Agremment (FLoA)
 
+library(fda)
+
 rm(list=ls())
 
-# Transform data to long format
+# Convert discrete time series to functional data (class fd (package fda))
 # ------------------------------------------------------------------------------
-data.long <- RawData_Long(n.subjects) # Data are interpolated to 101 data points and aggregated into joints and sides
+# data.long <- readRDS("data.long.rds")
+
+fd.basis <- create.fourier.basis(nbasis=50) # Function fit to empirical curves appears to plateau around 50 basis vectors
+
+# Returns delta curves, mean and sd for each joint condition
+# 6 joints, left / right == 18 list elements
+fd.fda.jointwise <- fdaData(data.long, fd.basis)
