1926bbe51d446106aa651cecdea6ed5cad7c75f8
Daniel Koska <danielkoska86@hotmail.com>
2021-02-01 16:43:03 +0100

    Rearrange structure of main script

--- R/main.R
+++ R/main.R
@@ -6,13 +6,10 @@ rm(list=ls())
 
 # Convert discrete time series to functional data (class fd (package fda))
 # ------------------------------------------------------------------------------
-# data.long <- readRDS("data.long.rds")
-
 fd.basis <- create.fourier.basis(nbasis=50) # Function fit to empirical curves appears to plateau around 50 basis vectors
 
-# Returns delta curves, mean and sd for each joint condition
-# 6 joints, left / right == 18 list elements
-fd.fda.jointwise <- fdaData(data.long, fd.basis)
+# data.long <- readRDS("data.long.rds")
+fd.fda.jointwise <- fdaData(data.long, fd.basis) # Returns delta curves, mean and sd for each condition
 
 # Randomized Cluster Bootstrap (FLoAboot_RCB)
 # ------------------------------------------------------------------------------
