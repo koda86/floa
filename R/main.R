c0887cf1bf2381583af28fc7897bc3ade6516a77
Daniel Koska <danielkoska86@hotmail.com>
2021-03-17 13:41:07 +0100

    Continued addition of functions

--- R/main.R
+++ R/main.R
@@ -4,8 +4,12 @@ library(fda)
 
 rm(list=ls())
 
+# Set wokring directories
+dir.data <- "C:/Users/Daniel/Desktop/tmp/floa/R"
+dir.script <- "C:/Users/Daniel/Desktop/tmp/floa/R/examples"
+
 # Long format data consisting of device, subjectID, and strideID
-# data <- readRDS("data.rds")
+data <- readRDS(paste0(dir.script, "/", "data.rds"))
 
 # Convert discrete time series to functional data (class fd (package fda))
 # ------------------------------------------------------------------------------
@@ -13,6 +17,9 @@ fd.basis <- create.fourier.basis(nbasis=50) # Function fit to empirical curves a
 
 # Randomized Cluster Bootstrap (FLoAboot_RCB)
 # ------------------------------------------------------------------------------
-floa_rcb <- floa_rcb(data, fd.basis, n.boot, n.subj)
+n.boot <- 5
 
-# test
+source("draw_clusters.R")
+source("floa_rcb.R")
+
+floa.rcb.percentiles.intrp <- floa_rcb(data, fd.basis, n.boot)
