56bd17863b750eb4720d067ceed344a496454323
Daniel Koska <danielkoska86@hotmail.com>
2021-03-17 14:38:42 +0100

    Minor formatting

--- R/main.R
+++ R/main.R
@@ -13,9 +13,19 @@ data <- readRDS(paste0(dir.script, "/", "data.rds"))
 
 # Convert discrete time series to functional data (class fd (package fda))
 # ------------------------------------------------------------------------------
-fd.basis <- create.fourier.basis(nbasis=50) # Function fit to empirical curves appears to plateau around 50 basis vectors
+# Function fit to empirical curves appears to plateau around 50 basis vectors
+fd.basis <- create.fourier.basis(nbasis=50)
 
-# Randomized Cluster Bootstrap (FLoAboot_RCB)
+# 1. Bootstrapped functional B & A Limits (Roislien et al., 2012) (FLoA_2SD)
+# ------------------------------------------------------------------------------
+# Returns 12 elements list with class fdata elements
+floa.boot.2SD.fdata <- FLOAboot_2SD(fd.fda.jointwise, n.boot)
+
+# Convert class 'fdata' to class 'funData' to prepare data for ggploting
+floa.boot.2SD.funData <- Conv2funData(floa.boot.2SD.fdata)
+
+
+# 2. Randomized Cluster Bootstrap (FLoAboot_RCB)
 # ------------------------------------------------------------------------------
 n.boot <- 5
 
