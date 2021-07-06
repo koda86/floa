c31f7d2ddeef29129bb4ee207ac5cdc65e29525a
Daniel Koska <danielkoska86@hotmail.com>
2021-07-06 11:49:07 +0200

    Minor formatting

--- R/main.R
+++ R/main.R
@@ -25,34 +25,25 @@ source("fdaDelta.R")
 
 ################################### Data sets ##################################
 
-# Wrapper function to call available data sets. Function arguments:
+# Wrapper function for example data sets.
+# Function arguments:
 # 1. Empirical validation data: "imu_mc"
 # 2. ARIMA: ...
 # 3. Fourier based surrogate data: ...
 
 data <- example_data(dat = "imu_mc", dir.data)
 
-
 ################################ Calculate FLoA ################################
 
 n.boot <- 100
 
-# Randomized Cluster Bootstrap (FLoAboot_RCB) -------------------------------
+# Randomized Cluster Bootstrap -------------------------------------------------
 #
 # Function returns difference curves and percentiles (2.5%, 50%, 97.5%)
 FLOArcb <- floa_rcb(data, plt = FALSE)
-# FLOArcb <- floa_rcb(data, fd.basis, n.boot, plt = FALSE) # Version with functional data
 
-
-
-
-# # FLoA_2SD ------------------------------------------------------------------
-# floa.boot.2SD.fdata <- FLOAboot_2SD(fda.delta, n.boot)
-#
-# # Convert class 'fdata' to class 'funData' to prepare data for ggploting
-# floa.boot.2SD.fd <- lapply(floa.boot.2SD.fdata, fdata2fd)
-# floa.boot.2SD.funData <- new("multiFunData", list(fd2funData(floa.boot.2SD.fd[[1]], argvals=seq(0, 1, 0.01)),
-#                                                   fd2funData(floa.boot.2SD.fd[[2]], argvals=seq(0, 1, 0.01))))
+# Version with functional data
+# FLOArcb <- floa_rcb(data, fd.basis, n.boot, plt = FALSE)
 
 
 
