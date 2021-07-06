4e496315047818582b9677a87f2dc2762ced5fc0
Daniel Koska <danielkoska86@hotmail.com>
2021-07-06 07:41:41 +0200

    Subsection data sets introduced

--- R/main.R
+++ R/main.R
@@ -22,13 +22,19 @@ source("draw_clusters.R")
 source("floa_rcb.R")
 source("fdaDelta.R")
 
+################################################################################
+################################### Data sets ##################################
+################################################################################
+
+# Real world IMU vs. MC validation data ----------------------------------------
 # Long format data consisting of device, subjectID, and strideID
 data <- readRDS(paste0(dir.data, "/", "data.rds"))
 
 # Intermediate step: Fit functions to discrete time series data ----------------
 fd.basis <- create.fourier.basis(nbasis=50) # Plateau around 50 basis vectors
-# Fit Fourier. Returns delta curves, mean and sd.
-fda.delta <- fdaDelta(data, fd.basis)
+fda.delta <- fdaDelta(data, fd.basis) # Fit Fourier, returns delta curves (mean, sd)
+
+# Surrogate data sets
 
 
 ################################################################################
@@ -37,20 +43,21 @@ fda.delta <- fdaDelta(data, fd.basis)
 
 n.boot <- 100
 
-# # 1. FLoA_2SD ------------------------------------------------------------------
+# Randomized Cluster Bootstrap (FLoAboot_RCB) -------------------------------
+#
+# Function returns difference curves and percentiles (2.5%, 50%, 97.5%)
+FLOArcb <- floa_rcb(data, fd.basis, n.boot, plt = FALSE)
+
+
+
+
+# # FLoA_2SD ------------------------------------------------------------------
 # floa.boot.2SD.fdata <- FLOAboot_2SD(fda.delta, n.boot)
 #
 # # Convert class 'fdata' to class 'funData' to prepare data for ggploting
 # floa.boot.2SD.fd <- lapply(floa.boot.2SD.fdata, fdata2fd)
 # floa.boot.2SD.funData <- new("multiFunData", list(fd2funData(floa.boot.2SD.fd[[1]], argvals=seq(0, 1, 0.01)),
 #                                                   fd2funData(floa.boot.2SD.fd[[2]], argvals=seq(0, 1, 0.01))))
-
-# 2. Randomized Cluster Bootstrap (FLoAboot_RCB) -------------------------------
-#
-# Function returns difference curves and percentiles (2.5%, 50%, 97.5%)
-FLOArcb <- floa_rcb(data, fd.basis, n.boot, plt = FALSE)
-
-# 3. Pointwise (Bland & Altman) (FLoA_Point) -----------------------------------
 
 
 
