b5cd57f9bda399e6aaaf1142e84109ae2300434e
koda86 <daniel.koska@hsw.tu-chemnitz.de>
2022-01-11 17:10:10 +0100

    Unified formatting

--- R/pick_subwise_curves.R
+++ R/pick_subwise_curves.R
@@ -1,10 +1,7 @@
 pick_subwise_curves <- function (data) {
 
-  # ****************************************************************************
-  # ******   Select a single random stride from every subject in data   ********
-  # ****************************************************************************
-
   # Select a single random stride from every subject in data
+
   n.subj <- length(unique(data$subjectID))
 
   curve.idx <- c()
