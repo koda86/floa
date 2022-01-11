a7d87d461767388c2ee58d1c92234c58414dcc06
koda86 <daniel.koska@hsw.tu-chemnitz.de>
2022-01-11 16:46:57 +0100

    Unified the formatting
    See tidyverse syle guide

--- R/example_data.R
+++ R/example_data.R
@@ -1,29 +1,19 @@
 example_data <- function(dat, dir.data) {
-
-  # Simulation outsourced to script 'simulate_data'
+  # Actual data preparation in script 'simulate_data'
 
   if (dat == "imu_mc") {
-
-    # IMU vs. MC validation data
     data <- readRDS(paste0(dir.data, "/", "imu_mc.rds"))
 
   } else if (dat == "smooth_realistic") {
-
     data <- readRDS(paste0(dir.data, "/", "smooth_realistic.rds"))
 
   } else if (dat == "non_gaussian") {
-
     data <- readRDS(paste0(dir.data, "/", "non_gaussian.rds"))
 
   } else if (dat == "shock") {
-
     data <- readRDS(paste0(dir.data, "/", "shock.rds"))
 
   } else if (dat == "shift") {
-
     data <- readRDS(paste0(dir.data, "/", "shift.rds"))
   }
-
-  return(data)
-
 }
