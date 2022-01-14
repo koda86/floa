159e5e56e89455b4b37371c3ffc5503fe9a70aef
koda86 <daniel.koska@hsw.tu-chemnitz.de>
2022-01-14 16:09:29 +0100

    Unified formatting

--- R/example_data.R
+++ R/example_data.R
@@ -5,18 +5,23 @@ example_data <- function(dat, dir.data) {
   # ****************************************************************************
 
   if (dat == "imu_mc") {
+
     data <- readRDS(paste0(dir.data, "/", "imu_mc.rds"))
 
   } else if (dat == "smooth_realistic") {
+
     data <- readRDS(paste0(dir.data, "/", "smooth_realistic.rds"))
 
   } else if (dat == "non_gaussian") {
+
     data <- readRDS(paste0(dir.data, "/", "non_gaussian.rds"))
 
   } else if (dat == "shock") {
+
     data <- readRDS(paste0(dir.data, "/", "shock.rds"))
 
   } else if (dat == "shift") {
+
     data <- readRDS(paste0(dir.data, "/", "shift.rds"))
   }
 }
