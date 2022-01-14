00c095c831b8f0d58b31f08aa4bde3d787980540
koda86 <daniel.koska@hsw.tu-chemnitz.de>
2022-01-14 11:07:01 +0100

    Renamed function and variable names

--- R/singlecurve_coverage_fraction.R
+++ R/singlecurve_coverage_fraction.R
@@ -1,11 +1,12 @@
-singlecurve_coverage_fraction <- function (data, n.boot) {
+coverage_loocv <- function (data, n.boot) { # singlecurve_coverage_fraction
 
-  # --------------------------------------------------------------------
-  # Leave-one (curve) out method to estimate the uncertainty in the
-  # achieved coverage (see Lenhoff et al. (1999))
-  # --------------------------------------------------------------------
+  # ****************************************************************************
+  # Leave-one (curve) out method to estimate the uncertainty in the achieved
+  # coverage (see Lenhoff et al. (1999))
+  # ****************************************************************************
 
-  # Convert continuous strideID to identical strideID's for repeated measures across subjects
+  # Convert continuous strideID to identical strideID's for repeated measures
+  # across subjects
   n.subj <- length(unique(data$subjectID))
   n.devices <- length(unique(data$device))
   strides.per.subject <- length(unique(data$strideID)) / length(unique(data$subjectID))
@@ -14,23 +15,31 @@ singlecurve_coverage_fraction <- function (data, n.boot) {
 
   n.curves <- unique(data$strideID)
 
-  cover.cross.point     <- vector(mode = "list", length = length(n.curves))
-  cover.cross.roislien  <- vector(mode = "list", length = length(n.curves))
-  cover.cross.boot      <- vector(mode = "list", length = length(n.curves))
-
+  cover.cross.fraction.point    <- vector(mode = "list", length = length(n.curves))
+  cover.cross.fraction.roislien <- vector(mode = "list", length = length(n.curves))
+  cover.cross.fraction.boot.rep <- vector(mode = "list", length = length(n.curves))
+  cover.cross.fraction.boot.iid <- vector(mode = "list", length = length(n.curves))
+  # Calculate LoA with one curve left out ------------------------------
   for (curve.idx in n.curves) {
-    # Calculate LoA with one curve left out ------------------------------
     data.one.out <- subset(data, strideID != curve.idx)
 
     floa.point    <- floa_point(data.one.out)
     floa.roislien <- floa_roislien(data.one.out)
-    floa.boot     <- floa_boot(data,
-                               k_reihe = 50,
+    floa.boot.rep <- floa_boot(data,
+                               k.coef = 50,
                                n.boot = n.boot,
                                band = "prediction",
                                cp.begin = 0,
                                alpha = 0.05,
                                iid = FALSE)
+    floa.boot.iid <- floa_boot(data,
+                               k.coef = 50,
+                               n.boot = n.boot,
+                               band = "prediction",
+                               cp.begin = 0,
+                               alpha = 0.05,
+                               iid = TRUE)
+
 
     # Plot left out curve vs. various methods ----------------------------
     data.subset <- subset(data, strideID == curve.idx)
@@ -39,18 +48,20 @@ singlecurve_coverage_fraction <- function (data, n.boot) {
     device2     <- data.frame(subset(data.subset, device == "MC"))
     device.diff <- device1$value - device2$value
 
-    # Get coverage for the left out (difference) curve
-    # --------------------------------------------------------------------
-    cover.cross.point[curve.idx]    <- get_coverage_singlecurve_fraction(device.diff, floa.point)
-    cover.cross.roislien[curve.idx] <- get_coverage_singlecurve_fraction(device.diff, floa.roislien)
-    cover.cross.boot[curve.idx]     <- get_coverage_singlecurve_fraction(device.diff, floa.boot)
+
+    # Get coverage for the left out (difference) curve -------------------
+    cover.cross.fraction.point[curve.idx]      <- get_coverage_singlecurve_fraction(device.diff, floa.point)
+    cover.cross.fraction.roislien[curve.idx]   <- get_coverage_singlecurve_fraction(device.diff, floa.roislien)
+    cover.cross.fraction.boot.rep[curve.idx]   <- get_coverage_singlecurve_fraction(device.diff, floa.boot.rep)
+    cover.cross.fraction.boot.iid[curve.idx]   <- get_coverage_singlecurve_fraction(device.diff, floa.boot.iid)
   }
 
-  cover.cross <- cbind(
-    unlist(cover.cross.point),
-    unlist(cover.cross.roislien),
-    unlist(cover.cross.boot)
+  cover.cross.fraction <- cbind(
+    unlist(cover.cross.fraction.point),
+    unlist(cover.cross.fraction.roislien),
+    unlist(cover.cross.fraction.boot.rep),
+    unlist(cover.cross.fraction.boot.iid)
     )
 
-  return(cover.cross)
+  return(cover.cross.fraction)
 }
