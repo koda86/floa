206601cebc9bfb2730de85adc438e59a9646c071
koda86 <daniel.koska@hsw.tu-chemnitz.de>
2022-01-14 13:41:53 +0100

    Unified formatting

--- R/coverage_curves.R
+++ R/coverage_curves.R
@@ -3,6 +3,7 @@ coverage_curves <- function(vector.cover, p.cover) {
   # ****************************************************************************
   # What is the proportion of bands (across cross validation iterations) that
   # contain at least p.cover of the T = 101 curve points?
+  # ----------------------------------------------------------------------------
   #
   # p.cover (0 < p.cover < 1) is the proportion of covered points per curve
   # vector.cover is a column vector with coverage percentages for each curve
